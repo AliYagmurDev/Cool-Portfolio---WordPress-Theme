e68636e8c13688fe14223e0b866e2d1a351922dc
AliYagmurGames <ravenperception@gmail.com>
2024-08-20 00:09:38 +0300

    The first setup of the theme, ready to work on

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body <?php body_class(); ?>>
+  <h1>BM Cool Portfolio</h1>
+  <div id="page" class="site">
+    <header>
+      <section class="top-bar">
+        <div class= "logo">
+
+        </div>
+        <div class="searchbox">
+          
+        </div>
+      </section>
+    </header>
+  </div>
+</body>
+</html>
